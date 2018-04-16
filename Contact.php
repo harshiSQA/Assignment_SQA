19ce2ca51df9a8edf2d8777f9eb3a29776a4afde
harshiSQA <37262164+harshiSQA@users.noreply.github.com>
2018-04-16 09:29:27 +0530

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,16 @@
+
+<?php
+if(isset($_POST['submit'])){
+
+	$fname=$_POST["fname"];
+	$lname=$_POST["lname"];
+	$age=$_POST["age"];
+	
+	echo "First Name :".$fname.'</br>';
+	echo "Last Name :".$lname.'</br>';
+	echo "Age :".$age.'</br>';
+
+	
+
+}
+?>
